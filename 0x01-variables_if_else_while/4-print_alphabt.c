c37ea6d94b98eb132167c95f57598e5280f41ad2
devMiiaHxy <dopemiiah@gmail.com>
2023-07-15 14:00:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  *main- Entry point of any program
+  *Description: 'Print every letter except q & e'
+  *Return: Always return 0
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(n);
+			n++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
