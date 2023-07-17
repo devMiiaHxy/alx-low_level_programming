120b35e9904425e9905395eb44bfc9f9ffa0d17e
devMiiaHxy <dopemiiah@gmail.com>
2023-07-17 16:01:41 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point of every program
+  *Description: Possible combination of 2 digit numbers
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
