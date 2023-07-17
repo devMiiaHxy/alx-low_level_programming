52c7ae23c68c7f81a73e9f045f34e9fe9002fa61
devMiiaHxy <dopemiiah@gmail.com>
2023-07-17 01:19:48 -0700

    6-print-numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point of every program
+  *Description: 'Print numberz'
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
