dbb4a38e261905d222a83f4c9b3147ab4138ee91
devMiiaHxy <dopemiiah@gmail.com>
2023-07-17 17:44:55 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Entry point of any program
+  *Description: 'Software is eating the world'
+  *Return: Always 0
+*/
+int main(void)
+{
+	int n = 120;
+
+	while (n < 140) 
+	{
+		putchar(n);
+		n++;
+	}
+		putchar('\n');
+		return (0);
+}
