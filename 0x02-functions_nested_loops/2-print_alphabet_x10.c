aae84cafb4e976a97dd8a76691ee56b2132c3067
devMiiaHxy <dopemiiah@gmail.com>
2023-07-19 12:18:23 -0700

    all files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
   */
 void print_alphabet_x10(void)
 {
-	char bx;
+	char bx = 0;
 	char by;
 
-	for (bx = 0; bx < 10; bx++)
+	while (bx < 10)
 	{
 		for (by = 'a'; by <= 'z'; by++)
 		{
 			_putchar(by);
 		}
-		_putchar(bx);
 		_putchar('\n');
+		bx++;
 	}
 }
