de84eb5b5dc47817d2a87d5b0c1a9acec20b21fe
devMiiaHxy <dopemiiah@gmail.com>
2023-07-20 08:37:08 -0700

    all files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _isdigit - Checks number from 0-9
+* @x: The number to be iterated over
+* Return: 1 for range 0-9 & 0 for diff range
+*/
+int _isdigit(int x)
+{
+if (x >= 0 && x <= 9)
+{
+return (1);
+}
+return (0);
+}
