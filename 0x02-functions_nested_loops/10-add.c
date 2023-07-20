93f82519183c86d80f52e1799f19144291326295
devMiiaHxy <dopemiiah@gmail.com>
2023-07-19 20:18:08 -0700

    all files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds 2 different values together
+ *@x1: first value to be added
+ *@x2: other value to be added
+ * Return: Number value
+ */
+int add(int x1, int x2)
+{
+return (x1 + x2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add_int(int, int);
+int add(int, int);
 void print_to_98(int n);
 
 #endif
