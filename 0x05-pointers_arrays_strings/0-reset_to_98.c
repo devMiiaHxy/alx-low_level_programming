760abbc65e3e1dbbfd1b7d922849f24b76825d3b
devMiiaHxy <meetmiiah@gmail.com>
2023-07-24 19:09:37 +0100

    all files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that accepts a pointer to an int ..
+ * @n: vaue to be iterated to 9
+ * Return: Always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
