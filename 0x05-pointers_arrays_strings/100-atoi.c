b415cf71192ff6ee084e7bb893510db761da4576
devMiiaHxy <meetmiiah@gmail.com>
2023-07-25 01:01:33 +0100

    all files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,40 +9,19 @@
 
 int _atoi(char *s)
 {
-	int x,d,n,len,f,digit;
+	unsigned int x = 0;
+	int y = 1;
 
-	x = 0;
-	d = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (x < len && f == 0)
-	{
-		if (s[x] == '-')
-			++d;
-		if (s[x] >= '0' && s[x] <= '9')
-		{
-			digit = s[x] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[x + 1] < '0' || s[x + 1] > '9')
-					break;
-				f = 0;
-		}
-		x++;
-
+	do {
+		if (*s == '-')
+			y *= -1;
+		else if (*s >= '0' && *s <= '9')
+			x = (x * 10) + (*s - '0');
+		else if (x > 0)
+			break;
 	}
+	while (*s++);
 
-	if (f == 0)
-		return(0);
-
-	return(n);
-
+	return (x * y);
 }
 
