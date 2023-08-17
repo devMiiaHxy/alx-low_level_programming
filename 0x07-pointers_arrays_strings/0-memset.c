386b6fb56ca82b373abce71c8c39633e1bb34b9f
devMiiaHxy <meetmiiah@gmail.com>
2023-08-17 11:08:42 +0100

    few adjustments made

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,19 +2,19 @@
 /**
  * _memset- add value to specfic block of memory
  * @x: memeory address to be filled first
- * @y: wanted value
- * @n: amount of bytes to be adjusted
+ * @y: char to copy
+ * @n: amount of times to copy 'y'
  *
  * Return: new array with new value
  */
 char *_memset(char *x, char y, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	for (; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
 		x[i] = y;
-		n--;
 	}
+	
 	return (x);
 }
