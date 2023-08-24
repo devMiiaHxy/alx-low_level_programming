ede84690c56ed5bb7de45e5b29b579dfe248ab1e
devMiiaHxy <onlyonemiiah@gmail.com>
2023-08-24 15:53:19 +0100

    all files

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - calculate factoial
+ * @n : parameter to take in
+ * Return: fatorial value
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		return (1);
+	}
+
+	return n * factorial(n - 1);
+}
